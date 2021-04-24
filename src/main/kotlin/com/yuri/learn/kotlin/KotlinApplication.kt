4c0bf2075d6353479ee8ea1e08defeb33dee31e7
Your Name <infonamina@gmail.com>
2021-04-24 03:00:48 +0100

    Initial project

---
+++ src/main/kotlin/com/yuri/learn/kotlin/KotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.yuri.learn.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinApplication>(*args)
+}
